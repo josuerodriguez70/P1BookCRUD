2b531f62b56c7f2121354b127827e62dca0cdebc
josuerodriguez70 <josuerodriguez70@outlook.com>
2022-03-10 22:17:07 -0600

    aspect changes
    aspect changes

--- src/main/java/spring2/aspect/AspectA.java
+++ src/main/java/spring2/aspect/AspectA.java
@@ -17,4 +17,28 @@ public class AspectA {
     public void BeforeGettingBook(JoinPoint joinPoint){
         logger.info(joinPoint.getSignature());
     }
+
+    @Before("execution(* postBook*(..))")
+    public void BeforePostingBook(JoinPoint joinPoint){
+        logger.info(joinPoint.getSignature());
+    }
+
+    @Before("execution(* postAuthor*(..))")
+    public void BeforePostingAuthor(JoinPoint joinPoint){
+        logger.info(joinPoint.getSignature());
+    }
+
+    @Before("execution(* getAuthor*(..))")
+    public void BeforeGettingAuthor(JoinPoint joinPoint){
+        logger.info(joinPoint.getSignature());
+    }
+
+    @Before("execution(* updatePrice*(..))")
+    public void BeforeUpdatePrice(JoinPoint joinPoint) {logger.info(joinPoint.getSignature());}
+
+    @Before("execution(* deleteBook*(..))")
+    public void BeforeDeleteBook(JoinPoint joinPoint) {logger.info(joinPoint.getSignature());}
+
+    @Before("execution(* deleteAuthor*(..))")
+    public void BeforeDeleteAuthor(JoinPoint joinPoint) {logger.info(joinPoint.getSignature());}
 }
